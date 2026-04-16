23646bb472f7e98919e5d843fb2df9931ed46c1b
mo2-alt <mohamednur2006@gmail.com>
2026-04-16 15:04:57 +0200

    ..

---
+++ app/src/main/java/com/example/topsoccerteams/MainActivity.kt
@@ -0,0 +1,58 @@
+package com.example.topsoccerteams
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.topsoccerteams.tostring
+import java.util.Arrays
+
+private val Unit.tostring: Any
+
+class MainActivity : AppCompatActivity(){
+
+
+    // Teams from https://footballdatabase.com/ranking/south-africa/1
+ // retrieved on 21 May 2022
+  val teams = arrayOf<String>("Mamelodi Sundowns FC", "Bidvest Wits", "Orlando Piates", "Kaizer chiefs", "Cape Town City FC")
+
+    // A new function
+    fun getshortstest(arr: Array<String>): String {
+        var shortest = arr[0]
+        for (team in arr) {
+            if (team.length < shortest.length) {
+                shortest = team
+            }
+        }
+    }
+
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState
+          setCintentView(R.layout.activity_main)
+
+           //get the eams text view
+        val TextView = findViewById<TextView>(R.id.topteams)
+        // set the text views text to the first team
+        val teamsTextView = null
+        teamsTextView.textview = teams[0] = "Mamelodi Sundowns FC <3"
+        // set the text views text to string representation  of teams
+        teamsTextView.text = Arrays.tostring.(teams)
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
